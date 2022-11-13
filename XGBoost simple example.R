0d9ed86d3b4b078bb223c5c85ba31391e157d3fc
ALLEN PARK <pmh621@naver.com>
2022-11-13 15:47:31 +0900

    Create XGBoost simple example.R

---
+++ XGBoost simple example.R
@@ -0,0 +1,44 @@
+install.packages('caret')
+set.seed(42)
+df <- data.frame(
+  Hours = c(0.50,0.75,1.00,1.25,1.50,1.75,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,4.00,4.25,4.50,4.75,5.00,5.50),
+  Pass = c(0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,1,1,1,1,1)
+)
+
+'%=%' = zeallot::`%<-%`
+
+library(caret, quietly=T)
+idx <- createDataPartition(df$Pass, list=F, p=0.8)
+c(train,test) %=% list(df[idx,],df[-idx,])
+
+#split train and test data
+c(train.data, test.data) %=% list(
+  as.matrix(train[,names(df)!="Pass"]),
+  as.matrix(test[,names(df)!="Pass"])
+)
+
+#split lables
+c(train.label, test.label) %=% list(train$Pass, test$Pass)
+
+library(xgboost)
+
+#rendering model
+model = xgboost(
+  data = train.data,
+  label = train.label,
+  max.depth = 2,
+  eta = 0.5,
+  nthread=2,
+  nrounds=2
+)
+
+#test
+pred = predict(model,test.data) 
+pred = ifelse(pred > 0.5,1,0)
+
+#accuracy
+sum(pred==test.label)/length(pred)
+
+
+
+
